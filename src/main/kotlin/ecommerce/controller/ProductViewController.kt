3cde1a74db5252a9486f5e288ed99888caa10afa
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-08-02 18:19:36 +0200

    fix: add missing newlines at the end of HTML files and remove unnecessary RequestMapping annotation

--- src/main/kotlin/ecommerce/controller/ProductViewController.kt
+++ src/main/kotlin/ecommerce/controller/ProductViewController.kt
@@ -13,10 +13,8 @@ import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping
 class ProductViewController(private val productService: ProductService) {
     @IgnoreCheckLogin
     @GetMapping
