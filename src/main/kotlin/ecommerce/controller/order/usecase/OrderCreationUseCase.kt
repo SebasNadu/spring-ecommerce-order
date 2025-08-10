b6bbfbec496be2228433a34cc34999ad46aecfac
SebasNadu <sebastiannavarro.fl@gmail.com>
2025-08-10 11:45:48 +0200

    feat(order): implement OrderController, OrderService, and related DTOs for order processing

---
+++ src/main/kotlin/ecommerce/controller/order/usecase/OrderCreationUseCase.kt
@@ -0,0 +1,9 @@
+package ecommerce.controller.order.usecase
+
+import ecommerce.dto.MemberLoginDTO
+import ecommerce.dto.OrderDTO
+import ecommerce.model.PaymentRequest
+
+interface OrderCreationUseCase {
+    fun create(memberLoginDTO: MemberLoginDTO, paymentRequest: PaymentRequest): OrderDTO
+}
---
+++ src/main/kotlin/ecommerce/mappers/OrderItemMapper.kt
@@ -0,0 +1,10 @@
+package ecommerce.mappers
+
+import ecommerce.entities.OrderItemEntity
+
+fun OrderItemEntity.toDTO() = ecommerce.dto.OrderItemDTO(
+    optionId = option.id,
+    quantity = quantity,
+    unitPrice = unitPrice,
+    id = id
+)
